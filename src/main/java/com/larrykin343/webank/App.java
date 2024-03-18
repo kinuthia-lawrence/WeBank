cf5b582614d5b4a6934c1b41a42c7c5709dd20f9
Kinuthia Lawrence <kinuthialawrence343@gmail.com>
2024-03-18 18:29:45 +0300

    added the stage title and the icon

--- src/main/java/com/larrykin343/webank/App.java
+++ src/main/java/com/larrykin343/webank/App.java
@@ -3,6 +3,7 @@ package com.larrykin343.webank;
 import javafx.scene.Scene;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class App extends Application {
@@ -11,6 +12,9 @@ public class App extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Fxml/Login.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setScene(scene);
+        stage.setTitle("WeBank");
+        Image icon = new Image(getClass().getResourceAsStream("/Images/vision.png"));
+        stage.getIcons().add(icon);
         stage.show();
 
     }
