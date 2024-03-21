a764f9d905976c75311465e51605fb29a4c9736f
Kinuthia Lawrence <kinuthialawrence343@gmail.com>
2024-03-21 05:41:23 +0300

    added the fxml injections

--- src/main/java/com/larrykin343/webank/Controller/LoginController.java
+++ src/main/java/com/larrykin343/webank/Controller/LoginController.java
@@ -1,13 +1,21 @@
 package com.larrykin343.webank.Controller;
 
+import javafx.fxml.FXML;
 import javafx.scene.control.*;
 
 public class LoginController {
+    @FXML
     public Label payment_address_label;
+    @FXML
     public ChoiceBox acc_selector;
+    @FXML
     public TextField payment_address_field;
+    @FXML
     public PasswordField Password_field;
+    @FXML
     public Label Password_label;
+    @FXML
     public Button Login_button;
+    @FXML
     public Label error_label;
 }
