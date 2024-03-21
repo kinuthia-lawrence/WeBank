078ecf7e01ffe7f67c80c15d60603ff73bc5b3be
Kinuthia Lawrence <kinuthialawrence343@gmail.com>
2024-03-21 05:52:17 +0300

    added the login imageView

--- src/main/java/com/larrykin343/webank/Controller/LoginController.java
+++ src/main/java/com/larrykin343/webank/Controller/LoginController.java
@@ -1,9 +1,16 @@
 package com.larrykin343.webank.Controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
-public class LoginController {
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginController implements Initializable {
     @FXML
     public Label payment_address_label;
     @FXML
@@ -18,4 +25,13 @@ public class LoginController {
     public Button Login_button;
     @FXML
     public Label error_label;
+    @FXML
+    public ImageView loginImageView;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        File imageFile = new File("src/main/resources/Images/vision.png");
+        Image loginImage = new Image(imageFile.toURI().toString());
+        loginImageView.setImage(loginImage);
+    }
 }
