4418bf1526922918a6c51197657cb8b4df378213
Kinuthia Lawrence <kinuthialawrence343@gmail.com>
2024-03-22 19:19:44 +0300

    modified the dashboard ui,gave them fxid and created injections

--- src/main/java/com/larrykin343/webank/Controller/Client/DashboardController.java
+++ src/main/java/com/larrykin343/webank/Controller/Client/DashboardController.java
@@ -1,4 +1,47 @@
 package com.larrykin343.webank.Controller.Client;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.text.Text;
+
 public class DashboardController {
+    @FXML
+    private TextField amountTextField;
+
+    @FXML
+    private Label checkingAccountNumber;
+
+    @FXML
+    private Label checkingBalance;
+
+    @FXML
+    private TextArea descriptionTextField;
+
+    @FXML
+    private Label expensesLabel;
+
+    @FXML
+    private Label incomeLabel;
+
+    @FXML
+    private Label loginDate;
+
+    @FXML
+    private TextField payeeAddressTextField;
+
+    @FXML
+    private Label savingsAccountNumber;
+
+    @FXML
+    private Label savingsBalance;
+
+    @FXML
+    private Button sendMoneyButton;
+
+    @FXML
+    private ListView<?> transactionListView;
+
+    @FXML
+    private Text usernameLabel;
+
 }
