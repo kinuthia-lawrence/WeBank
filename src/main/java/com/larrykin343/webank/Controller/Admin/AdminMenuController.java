d73a36aaf75564869adff76e49ce23eb21eec14e
Kinuthia Lawrence <kinuthialawrence343@gmail.com>
2024-03-24 06:18:02 +0300

    created the admin menu

--- src/main/java/com/larrykin343/webank/Controller/Admin/AdminMenuController.java
+++ src/main/java/com/larrykin343/webank/Controller/Admin/AdminMenuController.java
@@ -1,4 +1,19 @@
 package com.larrykin343.webank.Controller.Admin;
 
-public class AdminMenuController {
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AdminMenuController implements Initializable {
+    public Button logoutButton;
+    public Button depositButton;
+    public Button clientsButton;
+    public Button createClientsButton;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
