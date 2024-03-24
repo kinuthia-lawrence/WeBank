78b2c317a3a5d163ac786ee589695b534521dc93
Kinuthia Lawrence <kinuthialawrence343@gmail.com>
2024-03-24 23:05:52 +0300

    added the transactionCell view

--- src/main/java/com/larrykin343/webank/Controller/Client/TransactionCellController.java
+++ src/main/java/com/larrykin343/webank/Controller/Client/TransactionCellController.java
@@ -1,11 +1,20 @@
 package com.larrykin343.webank.Controller.Client;
 
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class TransactionCellController implements Initializable {
+    public ImageView receivedIcon;
+    public ImageView sentIcon;
+    public Label transactionDateLabel;
+    public Label senderLabel;
+    public Label receiverLabel;
+    public Label TransactionAmountLabel;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
--- src/main/java/com/larrykin343/webank/Controller/Client/TransactionsController.java
+++ src/main/java/com/larrykin343/webank/Controller/Client/TransactionsController.java
@@ -12,12 +12,7 @@ import java.util.ResourceBundle;
 public class TransactionsController implements Initializable {
     @FXML
     public ListView transactionsListView;
-    public ImageView receinvedIcon;
-    public ImageView sentIcon;
-    public Label transactionDateLabel;
-    public Label senderLabel;
-    public Label receiverLabel;
-    public Label TransactionAmoutLabel;
+
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
