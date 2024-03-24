0f8d6b0385dfefb8f72edc5b54cd628dd5b38242
Kinuthia Lawrence <kinuthialawrence343@gmail.com>
2024-03-25 01:37:10 +0300

    added the clientCell view and its controller

---
+++ src/main/java/com/larrykin343/webank/Controller/Client/ClientCellController.java
@@ -0,0 +1,17 @@
+package com.larrykin343.webank.Controller.Client;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
+public class ClientCellController {
+    public ImageView receivedIcon;
+    public Label firstNameLabel;
+    public Label lastNameLabel;
+    public Label checkingAccountNumberLabel;
+    public Label payeeAddressLabel;
+    public Label savingAccountNumberLabel;
+    public Label dateCreated;
+    public Button deleteButton;
+    public ImageView receivedIcon1;
+}
