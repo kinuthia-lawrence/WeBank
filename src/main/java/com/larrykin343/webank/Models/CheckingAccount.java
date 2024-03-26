d037914208e156a10d4cce844610e62939e5ce00
Kinuthia Lawrence <kinuthialawrence343@gmail.com>
2024-03-26 22:25:58 +0300

    modified checking Account model

--- src/main/java/com/larrykin343/webank/Models/CheckingAccount.java
+++ src/main/java/com/larrykin343/webank/Models/CheckingAccount.java
@@ -3,12 +3,16 @@ package com.larrykin343.webank.Models;
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 
-public class CheckingAccount extends Account{
+public class CheckingAccount extends Account {
     //the number of transactions allowed per month
     private final IntegerProperty transactionLimit;
 
     public CheckingAccount(String owner, String accountNumber, double balance, int transactionLimit) {
         super(owner, accountNumber, balance);
         this.transactionLimit = new SimpleIntegerProperty(this, "Transaction Limit", transactionLimit);
+    }
+
+    public IntegerProperty transactionLimitProperty() {
+        return transactionLimit;
     }
 }
