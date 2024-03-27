e33df31610b23de90a0fbdc4ba22a22e452f121b
Kinuthia Lawrence <kinuthialawrence343@gmail.com>
2024-03-27 21:18:53 +0300

    debbuged data type error in the savingAccount

--- src/main/java/com/larrykin343/webank/Models/SavingsAccount.java
+++ src/main/java/com/larrykin343/webank/Models/SavingsAccount.java
@@ -1,4 +1,16 @@
 package com.larrykin343.webank.Models;
 
-public class SavingsAccount {
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+
+public class SavingsAccount extends Account{
+    private final DoubleProperty withdrawalLimit;
+
+    public SavingsAccount(String owner, String accountNumber, double balance, double withdrawalLimit) {
+        super(owner, accountNumber, balance);
+        this.withdrawalLimit = new SimpleDoubleProperty(this, "Withdrawal Limit",withdrawalLimit);
+    }
+    public DoubleProperty withdrawalLimitProperty() {
+        return withdrawalLimit;
+    }
 }
